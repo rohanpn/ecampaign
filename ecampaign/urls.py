3d309a65acc70afc2a6d2aa4e842df652512b5d6
Rohan Nagalkar <rohan.nagalkar@vertisinfotech.com>
2015-09-28 17:23:31 +0530

    Added url patterns to register organisation.

--- ecampaign/urls.py
+++ ecampaign/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from organisation.views import RegisterFormValidate
+from organisation.views import OrganisationRegisterView
 
 
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'ecampaign.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='org_home'),
-    url(r'^register/$', TemplateView.as_view(template_name='register_org.html'), name='reg_org'),
-    url(r'^register/validate/$', RegisterFormValidate.as_view(), name='register'),
+    url(r'^register/$', TemplateView.as_view(template_name='register_org.html'), name='register_org'),
+    # url(r'^register/validate/$', RegisterView.as_view(), name='register'),
+    url(r'^register/validate/$', OrganisationRegisterView.as_view(), name='register'),
+    url(r'^success/$', TemplateView.as_view(template_name='organisation/success.html')),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
